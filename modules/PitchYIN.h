d68ef7a5b88db32e26b849276c552cfb7d3015cb
Adam Wilson <adam@codegarden.co.uk>
2015-11-25 19:20:21 +0000

    tidy up

--- modules/PitchYIN.h
+++ modules/PitchYIN.h
@@ -12,13 +12,15 @@ public:
 //        tolerence = 0.15;
 //    }
 
-    PitchYIN (unsigned int bufferSize) : yin (1, bufferSize), tolerence (0.15)
+    PitchYIN (unsigned int bufferSize) : yin (1, bufferSize), bufferSize (bufferSize), tolerence (0.15),
+    deltaWasNegative (false)
     {
     }
     
     void setSampleRate(unsigned int newSampleRate)
     {
         sampleRate = newSampleRate;
+        DBG ("sampleRate: " + String(sampleRate));
     }
 
     /** Output the difference function */
@@ -38,7 +40,6 @@ public:
                 yinData[tau] += (tmp * tmp);
             }
         }
-        
     }
 
     /** cumulative mean normalized difference function */
@@ -84,46 +85,59 @@ public:
         int period;
         float delta = 0.0, runningSum = 0.0;
         float *yinData = yin.getWritePointer(0);
+        //deltaWasNegative = false;
 
         //DBG ("calculatePitch");
 
         yinData[0] = 1.0;
-        for (int tau = 1; tau < yin.getNumSamples(); tau++) {
+        for (int tau = 1; tau < yin.getNumSamples(); tau++)
+        {
             yinData[tau] = 0.0;
-            for (int j = 0; j < yin.getNumSamples(); j++) {
+            for (int j = 0; j < yin.getNumSamples(); j++)
+            {
                 delta = inputData[j] - inputData[j + tau];
                 yinData[tau] += (delta * delta);
+                //if (delta < 0) deltaWasNegative = true;
             }
             runningSum += yinData[tau];
-            if (runningSum != 0) {
+            if (runningSum != 0)
+            {
                 yinData[tau] *= tau / runningSum;
-            } else {
+            }
+            else
+            {
                 yinData[tau] = 1.0;
             }
             period = tau - 3;
             if (tau > 4 && (yinData[period] < tolerence) &&
-                    (yinData[period] < yinData[period + 1])) {
-                return quadraticPeakPosition (yin, period);
+                    (yinData[period] < yinData[period + 1]))
+            {
+                DBG ("return early");
+                return quadraticPeakPosition (yin.getReadPointer(0), period);
             }
         }
-        return quadraticPeakPosition (yin, minElement (yin));
+        return quadraticPeakPosition (yin.getReadPointer(0), minElement (yin.getReadPointer(0)));
     }
 
     float getPitchInHz (const float* inputData) noexcept
     {
         float pitch = 0.0;
         //slideBlock (input);
-        //pitch = calculatePitch (input);
         pitch = calculatePitch (inputData);
-        //DBG ("pitch: " << pitch);
+        log->writeToLog("pitch: " + String(pitch));
         
-        if (pitch > 0) {
+        if (pitch > 0)
+        {
             pitch = sampleRate / (pitch + 0.0);
-        } else {
+            log->writeToLog ("pitchInHz: " + String(pitch));
+        }
+        else
+        {
             pitch = 0.0;
         }
         currentPitch = pitch;
-        //DBG ("pitchInHz: " << pitch);
+        //if (deltaWasNegative) DBG("delta negative");
+        
         return pitch;
     }
     
@@ -133,64 +147,67 @@ public:
     }
 
 private:
-    AudioSampleBuffer yin, buf;
+    AudioSampleBuffer yin; //, buf;
+    //float* yinData;
+    unsigned int bufferSize;
     unsigned int tolerence; //, confidence;
     unsigned int sampleRate;
+    bool deltaWasNegative;
     float currentPitch;
+    Logger *log;
 
-    /** adapter to stack ibuf new samples at the end of buf, and trim `buf` to `bufsize` */
-    void slideBlock (AudioSampleBuffer& ibuf)
-    {
-        float *bufData = buf.getWritePointer(0);
-        const float *ibufData = ibuf.getReadPointer(0);
-
-        unsigned int j = 0, overlapSize = 0;
-        overlapSize = buf.getNumSamples() - ibuf.getNumSamples();
-        for (j = 0; j < overlapSize; j++) 
-        {
-            bufData[j] = bufData[j + ibuf.getNumSamples()];
-        }
-        for (j = 0; j < ibuf.getNumSamples(); j++) 
-        {
-            bufData[j + overlapSize] = ibufData[j];
-        }
-    }
+//    /** adapter to stack ibuf new samples at the end of buf, and trim `buf` to `bufsize` */
+//    void slideBlock (AudioSampleBuffer& ibuf)
+//    {
+//        float *bufData = buf.getWritePointer(0);
+//        const float *ibufData = ibuf.getReadPointer(0);
+//
+//        unsigned int j = 0, overlapSize = 0;
+//        overlapSize = buf.getNumSamples() - ibuf.getNumSamples();
+//        for (j = 0; j < overlapSize; j++) 
+//        {
+//            bufData[j] = bufData[j + ibuf.getNumSamples()];
+//        }
+//        for (j = 0; j < ibuf.getNumSamples(); j++) 
+//        {
+//            bufData[j + overlapSize] = ibufData[j];
+//        }
+//    }
 
     // Below functions should go in a seperate utilities class
 
-    float quadraticPeakPosition (AudioSampleBuffer& x, unsigned int pos) noexcept
+    float quadraticPeakPosition (const float *data, unsigned int pos) noexcept
     {
-        float s0, s1, s2; 
-        const float *xData = x.getReadPointer(0);
+        float s0, s1, s2;
         unsigned int x0, x2;
-        if (pos == 0 || pos == x.getNumSamples() - 1) return pos;
+        if (pos == 0 || pos == bufferSize - 1) return pos;
         x0 = (pos < 1) ? pos : pos - 1;
-        x2 = (pos + 1 < x.getNumSamples()) ? pos + 1 : pos;
-        if (x0 == pos) return (xData[pos] <= xData[x2]) ? pos : x2;
-        if (x2 == pos) return (xData[pos] <= xData[x0]) ? pos : x0;
-        s0 = xData[x0];
-        s1 = xData[pos];
-        s2 = xData[x2];
+        x2 = (pos + 1 < bufferSize) ? pos + 1 : pos;
+        if (x0 == pos) return (data[pos] <= data[x2]) ? pos : x2;
+        if (x2 == pos) return (data[pos] <= data[x0]) ? pos : x0;
+        s0 = data[x0];
+        s1 = data[pos];
+        s2 = data[x2];
         return pos + 0.5 * (s0 - s2 ) / (s0 - 2.* s1 + s2);
     }
 
-    unsigned int minElement (AudioSampleBuffer& s) noexcept
+    unsigned int minElement (const float *data) noexcept
     {
-        const float *sData = s.getReadPointer(0);
     #ifndef JUCE_USE_VDSP_FRAMEWORK
         unsigned int j, pos = 0;
-        float tmp = sData[0];
-        for (j = 0; j < s.getNumSamples(); j++) {
-            pos = (tmp < sData[j]) ? pos : j;
-            tmp = (tmp < sData[j]) ? tmp : sData[j];
+        float tmp = data[0];
+        for (j = 0; j < bufferSize; j++)
+        {
+            pos = (tmp < data[j]) ? pos : j;
+            tmp = (tmp < data[j]) ? tmp : data[j];
         }
     #else
         float tmp = 0.0;
         unsigned int pos = 0;
         #if !DOUBLE_SAMPLES
-        vDSP_minvi(sData, 1, &tmp, (vDSP_Length *)&pos, s.getNumSamples());
+        vDSP_minvi(data, 1, &tmp, (vDSP_Length *)&pos, bufferSize);
         #else
-        vDSP_minviD(sData, 1, &tmp, (vDSP_Length *)&pos, s.getNumSamples());
+        vDSP_minviD(data, 1, &tmp, (vDSP_Length *)&pos, bufferSize);
         #endif
     #endif
         return pos;
